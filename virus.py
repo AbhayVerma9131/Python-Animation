b5fe9ebf272e8a735118ce6f54e50dd6176a0cba
Abhay Verma <99489290+AbhayVerma9131@users.noreply.github.com>
2023-05-17 08:18:35 +0530

    Add files via upload

---
+++ virus.py
@@ -0,0 +1,74 @@
+from turtle import *
+speed(10)
+hideturtle()
+color('cyan')
+bgcolor('black')
+b=150
+while b>0:
+    right(b)
+    backward(b*3)
+    b-=1
+speed(10)
+hideturtle()
+color('red')
+bgcolor('white')
+b=180
+while b>0:
+    right(b)
+    backward(b*3)
+    b-=1
+speed(10)
+hideturtle()
+color('green')
+bgcolor('white')
+b=150
+while b>0:
+    left(b)
+    forward(b*3)
+    b-=1
+speed(10)
+hideturtle()
+color('yellow')
+bgcolor('black')
+b = 180
+while b > 0:
+    left(b)
+    forward(b*3)
+    b -= 1
+speed(10)
+hideturtle()
+color('cyan')
+bgcolor('black')
+b = 150
+while b > 0:
+    right(b)
+    backward(b*3)
+    b -= 1
+speed(10)
+hideturtle()
+color('red')
+bgcolor('black')
+b = 180
+while b > 0:
+    right(b)
+    backward(b*3)
+    b -= 1
+speed(10)
+hideturtle()
+color('green')
+bgcolor('black')
+b = 150
+while b > 0:
+    left(b)
+    forward(b*3)
+    b -= 1
+speed(10)
+hideturtle()
+color('yellow')
+bgcolor('black')
+b = 180
+while b > 0:
+    left(b)
+    forward(b*3)
+    b -= 1
+done()
